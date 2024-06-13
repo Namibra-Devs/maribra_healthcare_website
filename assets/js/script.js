d910228aafaa1f790ca3af74afb6ed1910d98433
kindo <kindojnr58@gmail.com>
2024-06-13 07:58:10 +0000

    services nav images added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,42 @@
 // Activate Navs----------------------
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.navbar .btn');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const buttons = document.querySelectorAll('.navbar .btn');
 
-    buttons.forEach(button => {
-      button.addEventListener('click', function() {
-        buttons.forEach(btn => btn.classList.remove('active'));
-        this.classList.add('active');
-      });
-    });
-  });
+//     buttons.forEach(button => {
+//       button.addEventListener('click', function() {
+//         buttons.forEach(btn => btn.classList.remove('active'));
+//         this.classList.add('active');
+//       });
+//     });
+//   });
 
   // Slider------------------------------
-  let currentIndex = 0;
+//   let currentIndex = 0;
 
-function showSlide(index) {
-    const slides = document.querySelectorAll('.carousel-item');
-    if (index >= slides.length) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = slides.length - 1;
-    } else {
-        currentIndex = index;
-    }
-    const offset = -currentIndex * 100;
-    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
-}
+// function showSlide(index) {
+//     const slides = document.querySelectorAll('.carousel-item');
+//     if (index >= slides.length) {
+//         currentIndex = 0;
+//     } else if (index < 0) {
+//         currentIndex = slides.length - 1;
+//     } else {
+//         currentIndex = index;
+//     }
+//     const offset = -currentIndex * 100;
+//     document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+// }
 
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
+// function nextSlide() {
+//     showSlide(currentIndex + 1);
+// }
 
-function prevSlide() {
-    showSlide(currentIndex - 1);
-}
+// function prevSlide() {
+//     showSlide(currentIndex - 1);
+// }
 
 // Auto-slide every 3 seconds
 setInterval(() => {
     nextSlide();
-}, 3000);
+}, 300);
 
 // Modal----------------
-<script>
-document.getElementById('openModalBtn').addEventListener('click', function () {
-    var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
-        keyboard: false
-    });
-    myModal.show();
-});
-</script>
